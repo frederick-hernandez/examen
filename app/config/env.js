8ce2245474a7a7a0c9afebc9554a527a0e578ff2
frederick-hernandez <53789782+frederick-hernandez@users.noreply.github.com>
2024-09-21 14:20:49 -0600

    abc

--- app/config/env.js
+++ app/config/env.js
@@ -2,7 +2,7 @@ const env = {
     database: 'umgppruebas',
     username: 'frhand',
     password: 'Dum3KaI5f6qqecW5W3MzEPAdRQg1W4L8',
-    host: 'dpg-cr6i082j1k6c73d4kr9g-aoregon-postgres.render.com',
+    host: 'dpg-cr6i082j1k6c73d4kr9g-a',
     dialect: 'postgres',
     pool: {
       max: 5,
